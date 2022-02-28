664511c95d4152f282bf860cf445a0c9e0ef6328
Furtssss <99706111+Furtssss@users.noreply.github.com>
2022-02-28 09:36:45 +0800

    Add files via upload

---
+++ midterms-demo-2/getmethod.js
@@ -0,0 +1,30 @@
+/* Activity Name: getmethod.js
+Author Name: Furton, Mark Lemuel B.
+Section and Code: WD-202 / 2083
+*/
+
+const { response } = require("express");
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.get("/indexget.html", function (req, res) {
+  res.sendFile(__dirname + "/" + "indexget.html");
+});
+
+app.get("/process_get", function (req, res) {
+  response = {
+    first_name: req.query.first_name,
+    last_name: req.query.last_name,
+  };
+
+  console.log(response);
+  res.end(JSON.stringify(response));
+});
+
+var server = app.listen(5000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+});
